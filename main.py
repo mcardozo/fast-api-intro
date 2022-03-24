a1f9e4ae17b50cb1264656b141d8346b7274dbdb
Marisol <cardozomarisolp@gmail.com>
2022-03-24 16:29:09 -0300

    validate path parameters

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 # FastAPI
-from fastapi import Body, FastAPI, Query
+from fastapi import Body, FastAPI, Query, Path
 
 # Models
 class Person(BaseModel):
@@ -27,15 +27,36 @@ def home():
     """Home page."""
     return {'hello': 'world'}
 
+
 @app.post('/person/new')
 def create_person(person: Person = Body(...)):
     """Create a person."""
     return person
 
+
 @app.get('/person/detail')
 def show_person(
-        name: Optional[str] = Query(default=None, min_length=1, max_length=50),
+        name: Optional[str] = Query(
+            None,
+            min_length=1,
+            max_length=50,
+            title='Persona name',
+            description='This is the persona name. It is between 1 and 50 characters.'
+        ),
         age: int = Query(...)
 ):
     """Validate query parameters."""
     return {name: age}
+
+
+@app.get('/person/detail/{person_id}')
+def show_person_detail(
+        person_id: int = Path(
+            ...,
+            gt=0,
+            title='Person',
+            description='Showing person with id',
+        ),
+):
+    """Return a person detail."""
+    return {person_id: "It exist!"}
