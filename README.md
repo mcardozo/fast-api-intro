d43ff19c6e56e7c6c63e6df747f42852b8fb2717
Marisol <cardozomarisolp@gmail.com>
2022-03-24 16:03:56 -0300

    added Person model

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 1. Run service
 
 		(venv)$ uvicorn main:app --reload
+
+2. See documetation through Swagger UI Request URL: `127.0.0.1:8000/docs` or through Redoc `127.0.0.1:8000/redoc`
--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 """Main module."""
 
-from fastapi import FastAPI
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
+from fastapi import Body, FastAPI
+
+# Models
+class Person(BaseModel):
+    """Person model."""
+
+    first_name : str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 
 app = FastAPI()
@@ -9,3 +26,8 @@ app = FastAPI()
 def home():
     """Home page."""
     return {'hello': 'world'}
+
+@app.post('/person/new')
+def create_person(person: Person = Body(...)):
+    """Create a person."""
+    return person
